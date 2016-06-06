28d579608021f72b0368d5fd31ef4acd0b3b4453
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2016-06-06 13:28:02 -0700

    update

--- lebab.py
+++ lebab.py
@@ -20,47 +20,6 @@ IS_WINDOWS = platform.system() == 'Windows'
 
 class LebabCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    # Save the current viewport position to scroll to it after formatting.
-    previous_selection = list(self.view.sel()) # Copy.
-    previous_position = self.view.viewport_position()
-
-    # Save the already folded code to refold it after formatting.
-    # Backup of folded code is taken instead of regions because the start and end pos
-    # of folded regions will change once formatted.
-    folded_regions_content = [self.view.substr(r) for r in self.view.folded_regions()]
-
-    # Get the current text in the buffer and save it in a temporary file.
-    # This allows for scratch buffers and dirty files to be linted as well.
-    entire_buffer_region = sublime.Region(0, self.view.size())
-    text_selection_region = self.view.sel()[0]
-
-    buffer_text = self.get_buffer_text(entire_buffer_region)
-
-    output = self.run_script_on_text(buffer_text)
-
-    # If the prettified text length is nil, the current syntax isn't supported.
-    if output == None or len(output) < 1:
-      return
-
-    # Replace the text only if it's different.
-    if output != buffer_text:
-      self.view.replace(edit, entire_buffer_region, output)
-
-    self.refold_folded_regions(folded_regions_content, output)
-    self.view.set_viewport_position((0, 0), False)
-    self.view.set_viewport_position(previous_position, False)
-    self.view.sel().clear()
-
-    # Restore the previous selection if formatting wasn't performed only for it.
-    # if not is_formatting_selection_only:
-    for region in previous_selection:
-      self.view.sel().add(region)
-
-  def get_buffer_text(self, region):
-    buffer_text = self.view.substr(region)
-    return buffer_text
-
-  def run_script_on_text(self, data):
     try:
       node_path = PluginUtils.get_node_path()
       lebab_path = PluginUtils.get_lebab_path()
@@ -71,52 +30,18 @@ class LebabCommand(sublime_plugin.TextCommand):
 
       cmd = [node_path, lebab_path, self.view.file_name(), '-o', self.view.file_name()]
 
-      config_path = PluginUtils.get_pref("config_path")
-      if config_path:
-        print("Using configuration from {0}".format(config_path))
-        cmd.extend(["--config", config_path])
-
       if self.view.file_name():
           cdir = os.path.dirname(self.view.file_name())
       else:
           cdir = "/"
 
-      output = PluginUtils.get_output(cmd, cdir, data)
-
-      return output;
+      PluginUtils.exec(cmd, cdir)
 
     except:
       # Something bad happened.
       msg = str(sys.exc_info()[1])
       print("Unexpected error({0}): {1}".format(sys.exc_info()[0], msg))
       sublime.error_message(msg)
-
-  def refold_folded_regions(self, folded_regions_content, entire_file_contents):
-    self.view.unfold(sublime.Region(0, len(entire_file_contents)))
-    region_end = 0
-
-    for content in folded_regions_content:
-      region_start = entire_file_contents.index(content, region_end)
-      if region_start > -1:
-        region_end = region_start + len(content)
-        self.view.fold(sublime.Region(region_start, region_end))
-
-class JscsFormatterEventListeners(sublime_plugin.EventListener):
-  @staticmethod
-  def on_pre_save(view):
-    if PluginUtils.get_pref("format_on_save"):
-      extensions = PluginUtils.get_pref("format_on_save_extensions")
-      extension = os.path.splitext(view.file_name())[1][1:]
-
-      # Default to using filename if no extension
-      if not extension:
-        extension = os.path.basename(view.file_name())
-
-      # Skip if extension is not whitelisted
-      if extensions and not extension in extensions:
-        return
-
-      view.run_command("format_javascript")
 
 class PluginUtils:
   @staticmethod
@@ -143,19 +68,19 @@ class PluginUtils:
   @staticmethod
   def get_lebab_path():
     platform = sublime.platform()
-    jscs = PluginUtils.get_pref("lebab_path").get(platform)
-    print("Using jscs path on '" + platform + "': " + jscs)
-    return jscs
+    lebab = PluginUtils.get_pref("lebab_path").get(platform)
+    print("Using lebab path on '" + platform + "': " + lebab)
+    return lebab
 
   @staticmethod
-  def get_output(cmd, cdir, data):
+  def exec(cmd, cdir):
     try:
       p = Popen(cmd,
         stdout=PIPE, stdin=PIPE, stderr=PIPE,
         cwd=cdir, shell=IS_WINDOWS)
     except OSError:
       raise Exception('Couldn\'t find Node.js. Make sure it\'s in your $PATH by running `node -v` in your command-line.')
-    stdout, stderr = p.communicate(input=data.encode('utf-8'))
+    stdout, stderr = p.communicate()
     stdout = stdout.decode('utf-8')
     stderr = stderr.decode('utf-8')
 
